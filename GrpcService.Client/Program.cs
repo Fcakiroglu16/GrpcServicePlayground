a3715a8ff43860d161cf9cd8d37f9a0f2009a8a0
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-06-05 16:09:57 +0300

    GetAll() method call completed

---
+++ GrpcService.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
+using Samples.gRPC.v1;
+AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+    builder.SetMinimumLevel(LogLevel.Error);
+});
+
+
+var channel = GrpcChannel.ForAddress("http://localhost:5000",new GrpcChannelOptions()
+{
+    LoggerFactory = loggerFactory
+});
+
+
+
+var countryClient= new CountryService.CountryServiceClient(channel);
+
+using var serverStreamingCall= countryClient.GetAll(new Empty());
+await foreach (var response in serverStreamingCall.ResponseStream.ReadAllAsync())
+{
+    Console.WriteLine($"{response.Name} {response.Description}");
+}
+
